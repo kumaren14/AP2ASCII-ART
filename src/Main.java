7550aa65de6bc69aa35f4cd424f549a7b8aecf61
kumaren14 <kumaren.a@gmail.com>
2022-01-11 08:13:29 -0600

    ASCII Art

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+
+    public static void main(String[] args) {
+    /*this was my absolutely horrific attempt to make a bored ape nft drawing.
+    the fact that it looks similar is a testament to how stupid and ugly those are.
+     */
+    System.out.println("    ∩    ∩");
+	System.out.println("\t|\t|");
+	System.out.println("     (..)");
+	System.out.println("     |");
+	System.out.println("     ____\n\n");
+	//how about a smiley face?
+	System.out.println("\t|\t|");
+	System.out.println("  \\_______/\n\n");
+	//just a plain little face
+	System.out.println("\t| |");
+	System.out.println("\t ∠");
+	System.out.println("\t -\n\n");
+	//now i'm going to make an inset diamond
+	System.out.println("***********");
+	System.out.println("*****#*****");
+	System.out.println("****###****");
+	System.out.println("***#####***");
+	System.out.println("**#######**");
+	System.out.println("*#########*");
+	System.out.println("###########");
+	System.out.println("*#########*");
+	System.out.println("**#######**");
+	System.out.println("***#####***");
+	System.out.println("****###****");
+	System.out.println("*****#*****");
+	System.out.println("***********\n\n");
+	//this should give us a little house for the people we made up above
+	System.out.println("       /#\\     ");
+	System.out.println("     /#####\\   ");
+	System.out.println("   /#########\\ ");
+	System.out.println(" /#############\\");
+	System.out.println("   |%%%%%%%%|");
+	System.out.println("   |%%%%%%%%|");
+	System.out.println("   |%%%  %%%|");
+	System.out.println("   |%%% .%%%|");
+	System.out.println("   |%%%  %%%|");
+	System.out.println("   ----------\n\n");
+	//and lets give the house a resident
+	System.out.println(" O");
+	System.out.println("/|\\");
+	System.out.println(" |");
+	System.out.println(" /\\");
+
+
+	int x = 100;
+	if(x < 100) {
+		System.out.println("less than a hundred");
+		System.out.println("increase the count");
+	}
+	if(x > 100) {
+		System.out.println("greater than a hundred");
+		System.out.println("decrease the count");
+	}
+	if(x != 100) {
+		System.out.println("does not equal a hundred");
+		System.out.println("make it equal a hundred");
+	} else {
+		System.out.println("equals a hundred");
+		System.out.println("good job!");
+	}
+    }
+}
